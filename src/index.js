f754a725d061c802c7a0a23a2cc6ec96f4516b9e
Dina Singatulina <dina.sing1212@gmail.com>
2019-09-23 20:33:07 +0300

    implement decoding

--- src/index.js
+++ src/index.js
@@ -37,8 +37,34 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(encodedString) { 
+    let array = encodedString.split('');
+    let encodedLetters = [];
+    let symbols = [];
+    let morseLetters = [];
+    let decodedString;
+    
+    for (let i = 2; i < array.length + 2; i+=2)
+    {
+        encodedLetters.push(array.slice(i-2, i).join(''));
+    }
+
+    symbols = encodedLetters.map(item => {
+        if(item == '**') return '*';
+        else if(item == '10') return '.';
+        else if (item == '11') return '-';
+        else return '';
+    })    
+    for (let i = 5; i < symbols.length + 5; i += 5)
+    {
+        morseLetters.push(symbols.slice(i-5, i).join(''));
+
+    }
+    decodedString = morseLetters.map(code => {
+        if(code == '*****') return ' ';
+        else return MORSE_TABLE[code];         
+    });
+    return decodedString.join('');    
 }
 
 module.exports = {
